783d9342fd2db20fa7b48a23a414d430677790d5
Scott Phillips <polygonguru@gmail.com>
2025-06-17 12:48:46 +0000

    build: Newer code in the right location

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@
 # npm install @openapitools/openapi-generator-cli -g
 openapi-generator-cli generate \
     -i ../../cw-api.json \
-    -g go -o ../../build/ \
-    --additional-properties packageName=cwapi \
+    -g go -o ../../ \
+    -c config.yaml \
     --git-user-id ai-connor \
     --git-repo-id connectwise
